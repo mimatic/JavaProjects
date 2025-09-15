c7eed86ed74e195ae3c5b04326a94546ac1adc24
tamimquadir <tq45@drexel.edu>
2025-09-15 13:42:59 -0400

    First Commit

---
+++ src/com/Main.java
@@ -0,0 +1,12 @@
+package com;
+
+public class Main {
+
+    // always static
+    // void doesn't return anything
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+
+    }
+}
